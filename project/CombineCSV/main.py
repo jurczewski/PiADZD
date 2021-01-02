a87f93f9e6018d784bbc85ea966589db8b5912aa
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-01-02 18:19:36 +0100

    Add time to combineCSV script

--- project/CombineCSV/main.py
+++ project/CombineCSV/main.py
@@ -1,6 +1,7 @@
 from argument_parser import ArgumentParser
 import os
 import pandas as pd
+from datetime import datetime
 
 
 argument_parser = ArgumentParser()
@@ -9,7 +10,10 @@ outputName = argument_parser.get_outputName()
 
 df = pd.DataFrame()
 for filename in os.listdir(directory):
-    if filename.endswith(".csv"): 
+    if filename.endswith(".csv"):
+        print(filename + ', time: ' + str(datetime.now().strftime("%H:%M:%S")))
         file = os.path.join(directory, filename)
         df = df.append(pd.read_csv(file), ignore_index=True)
+        print(filename + '_END, time: ' +
+              str(datetime.now().strftime("%H:%M:%S")))
 df.to_csv(outputName)
