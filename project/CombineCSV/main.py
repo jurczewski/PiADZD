fc3ab46d3bd4e6124b53be069c5fb6d7237367dc
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-01-02 18:40:27 +0100

    Add time to scirpt

--- project/CombineCSV/main.py
+++ project/CombineCSV/main.py
@@ -11,9 +11,10 @@ outputName = argument_parser.get_outputName()
 df = pd.DataFrame()
 for filename in os.listdir(directory):
     if filename.endswith(".csv"):
-        print(filename + ', time: ' + str(datetime.now().strftime("%H:%M:%S")))
+        time = str(datetime.now().strftime("%H:%M:%S"))
+        print(f'{filename}, time: {time}')
         file = os.path.join(directory, filename)
         df = df.append(pd.read_csv(file), ignore_index=True)
-        print(filename + '_END, time: ' +
-              str(datetime.now().strftime("%H:%M:%S")))
+        time = str(datetime.now().strftime("%H:%M:%S"))
+        print(f'{filename}_END, time: {time}')
 df.to_csv(outputName)
